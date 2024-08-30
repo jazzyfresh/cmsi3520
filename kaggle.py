4ced67d3fd15b826db0a95b0c459e62364b1b46c
Jazzy <jasmine.dahilig@gmail.com>
2024-08-29 17:36:19 -0700

    removed extra line

--- kaggle.py
+++ kaggle.py
@@ -1,20 +1,18 @@
 import pandas as pd
 import sqlite3
 
+# read csv file into memory using pandas
 df = pd.read_csv("amazon_laptop_prices_v01.csv")
 print(df.head())
 print(df.tail())
 
-
-
+# connect to sqlite database
+# this connects to an exiting file or creates a new file
 connection = sqlite3.connect("laptops.db")
 cursor = connection.cursor()
-cursor.execute("DROP TABLE IF EXISTS laptops")
-# create table
 
+# load data into the table
 df.to_sql("laptops", connection)
 
 connection.close()
-
-# load data into the table
 
