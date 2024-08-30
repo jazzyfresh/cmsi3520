cea6d0cd44138fcad9300e909299d7c154bf008b
Jazzy <jasmine.dahilig@gmail.com>
2024-08-29 17:45:27 -0700

    added comments to wiki scrape

--- sqlite/scrape.py
+++ sqlite/scrape.py
@@ -2,15 +2,24 @@ import mechanicalsoup
 import pandas as pd
 import sqlite3
 
+# scrape.py:
+# Download the URL
+# Parse the table entries into arrays
+# Create a DataFrame
+# Load into SQLite
+
+
+# Download the URL #
 url = "https://en.wikipedia.org/wiki/Comparison_of_Linux_distributions"
 browser = mechanicalsoup.StatefulBrowser()
 browser.open(url)
 
+
+# Parse the table entries into arrays #
 th = browser.page.find_all("th", attrs={"class": "table-rh"})
 distribution = [value.text.replace("\n", "") for value in th]
 # print(distribution.index("Zorin OS"))
 distribution = distribution[:98]
-
 # print(distribution)
 
 td = browser.page.find_all("td")
@@ -22,6 +31,8 @@ columns = [value.text.replace("\n", "") for value in td]
 columns = columns[6:1084]
 print(columns)
 
+
+# Create a DataFrame #
 column_names = ["Founder",
                 "Maintainer",
                 "Initial_Release_Year",
@@ -43,11 +54,12 @@ dictionary = {"Distribution": distribution}
 for idx, key in enumerate(column_names):
     dictionary[key] = columns[idx:][::11]
 
-
 df = pd.DataFrame(data = dictionary)
 print(df.head())
 print(df.tail())
 
+
+# Load into SQLite #
 connection = sqlite3.connect("distro.db")
 cursor = connection.cursor()
 cursor.execute("create table linux (Distribution, " + ",".join(column_names) + ")")
