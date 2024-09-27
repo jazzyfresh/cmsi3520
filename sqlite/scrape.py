63cc12b747ff9941d05332d6ac9fed34c4662afb
Jazzy <jasmine.dahilig@gmail.com>
2024-09-26 17:09:15 -0700

    simplified sqlite example with movies database

--- sqlite/scrape.py
+++ sqlite/scrape.py
@@ -1,70 +1,64 @@
+## scrape.py:
+##   Download the URL
+##   Parse the table entries into arrays
+##   Create a DataFrame
+##   Load into SQLite
+
 import mechanicalsoup
 import pandas as pd
 import sqlite3
 
-# scrape.py:
-# Download the URL
-# Parse the table entries into arrays
-# Create a DataFrame
-# Load into SQLite
 
 
 # Download the URL #
-url = "https://en.wikipedia.org/wiki/Comparison_of_Linux_distributions"
+url = "https://en.wikipedia.org/wiki/List_of_highest-grossing_films"
 browser = mechanicalsoup.StatefulBrowser()
 browser.open(url)
 
 
 # Parse the table entries into arrays #
-th = browser.page.find_all("th", attrs={"class": "table-rh"})
-distribution = [value.text.replace("\n", "") for value in th]
-# print(distribution.index("Zorin OS"))
-distribution = distribution[:98]
-# print(distribution)
+# table > td
+# table > tbody > th
 
-td = browser.page.find_all("td")
-columns = [value.text.replace("\n", "") for value in td]
-# print(columns)
-# print(columns.index("AlmaLinux Foundation"))
-# print(columns.index("Binary blobs"))
+# browser.page.find() # gives first instance of an html element
+# browser.page.find_all() # gives all instances
+# browser.page.find_all("table", attrs={"class":"wikitable"})
 
-columns = columns[6:1084]
-print(columns)
+table = browser.page.find("table")
+#tbody = table.find("tbody")
+# th = tbody.find_all("th")
+th = table.find_all("th")
+td = table.find_all("td")
+td = [value.text.replace("\n", "") for value in td]
+titles = [value.text.replace("\n", "") for value in th]
+titles = titles[6:]
 
 
 # Create a DataFrame #
-column_names = ["Founder",
-                "Maintainer",
-                "Initial_Release_Year",
-                "Current_Stable_Version", 
-                "Security_Updates", 
-                "Release_Date", 
-                "System_Distribution_Commitment", 
-                "Forked_From", 
-                "Target_Audience", 
-                "Cost", 
-                "Status"]
+column_names = ["rank", "peak", "world_wide_gross", "year", "ref"]
+
+dictionary = {"title": titles}
 
 # column[0:][::11]
 # column[1:][::11]
 # column[2:][::11]
 
-dictionary = {"Distribution": distribution}
 
 for idx, key in enumerate(column_names):
-    dictionary[key] = columns[idx:][::11]
+    dictionary[key] = td[idx:][::5]
 
 df = pd.DataFrame(data = dictionary)
 print(df.head())
 print(df.tail())
 
 
+
 # Load into SQLite #
-connection = sqlite3.connect("distro.db")
+connection = sqlite3.connect("movies.db")
 cursor = connection.cursor()
-cursor.execute("create table linux (Distribution, " + ",".join(column_names) + ")")
+cursor.execute("create table movies (title," + ",".join(column_names) + ")")
 for i in range(len(df)):
-    cursor.execute("insert into linux values (?,?,?,?,?,?,?,?,?,?,?,?)", df.iloc[i])
+    cursor.execute("insert into movies values (?,?,?,?,?,?)", df.iloc[i])
 
 connection.commit()
 
